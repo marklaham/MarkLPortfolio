590d79a74407db83e0201f9d87a00a555e5e7e7f
Mark Laham <marklaham@gmail.com>
2021-07-29 13:10:45 -0400

    fixed navbar and made it responsive

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,53 +1,51 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import './styles/Header.css';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-            <li className="nav-item">
-        <a
-          href="#about"
+
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#about"
           onClick={() => handlePageChange('About')}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#projects"
+          About</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link  href="#projects"
           onClick={() => handlePageChange('Projects')}
           className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
         >
           Projects
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
+        </Nav.Link>
+      <Nav.Link  href="#resume"
           onClick={() => handlePageChange('Resume')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
-          Resume
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
+          Resume</Nav.Link>
+    <Nav.Link  href="#contact"
           onClick={() => handlePageChange('Contact')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
-          Contact Me
-        </a>
-      </li>
-    </ul>
-  );
+          Contact Me</Nav.Link>
+    </Nav>
+
+  </Navbar.Collapse>
+  </Container>
+</Navbar>);
+  
+  
 }
 
 export default NavTabs;
